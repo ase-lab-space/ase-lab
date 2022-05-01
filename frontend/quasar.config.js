3fddcf5983e395bf3a3022ec3d60c79809b9d5c8
Hiroaki Honda <82698421+Damin3927@users.noreply.github.com>
2022-05-01 13:24:36 +0900

    Remove icon description

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -137,40 +137,14 @@ module.exports = configure(function (ctx) {
       // chainWebpackCustomSW (/* chain */) {},
 
       manifest: {
-        name: 'ASE-Lab. HP',
-        short_name: 'ASE-Lab. HP',
+        name: 'ASE-Lab. Home',
+        short_name: 'ASE-Lab. Home',
         description: '',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
         theme_color: '#027be3',
-        icons: [
-          {
-            src: 'icons/icon-128x128.png',
-            sizes: '128x128',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
+        icons: [],
       },
     },
 
