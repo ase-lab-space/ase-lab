0ddfe9bf727446797cc27ab8dc60768a0121315c
Hiroaki Honda <82698421+Damin3927@users.noreply.github.com>
2022-05-01 12:55:49 +0900

    Move meta data into the model directory

--- frontend/src/components/AboutPage/MapSection.vue
+++ frontend/src/components/AboutPage/MapSection.vue
@@ -27,36 +27,7 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import anime from 'animejs';
 import SlideIn from '../Common/Transition/SlideIn.vue';
-
-const participantNumber = 67;
-
-const univs: string[] = [
-  '東京大学',
-  '早稲田大学',
-  '東北大学',
-  '東京工業大学',
-  'お茶の水女子大学',
-  '慶應大学',
-  '明治大学',
-  '横浜国立大学',
-  '東京理科大学',
-  '国際教養大学',
-  '名古屋工業大学',
-  '京都産業大学',
-  '大阪府立大学',
-  '帝京大学',
-  '岐阜大学',
-  '信州大学',
-  '弘前大学',
-  '日本大学',
-  '法政大学',
-  '東海大学',
-  '鳥取大学',
-  '東京学芸大学',
-  '九州大学',
-  '九州工業大学',
-  'トロント大学',
-];
+import { participantNumber, univs } from 'src/models/meta';
 
 export default defineComponent({
   components: {
