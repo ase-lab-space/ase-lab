ceeb279443bb4928a3eb71991fe6b5f8e3051bf3
TakeshiTakamizawa <142018275+TakeshiTakamizawa@users.noreply.github.com>
2023-08-19 19:28:19 +0900

    Update seminars.ts
    ゼミの状態を更新(宇宙工学入門ゼミ)

--- src/models/seminars.ts
+++ src/models/seminars.ts
@@ -35,6 +35,15 @@ export interface ISeminar {
 }
 
 export const seminars: ISeminar[] = [
+   {
+    name: '宇宙工学入門ゼミ',
+    description:
+      '今回のゼミの狙いは、「宇宙工学の基礎知識を身につけること」です。宇宙分野に興味がある人で、何から学べばいいのか迷っている人、学部１、２年生で基礎知識を身につけたい人など、ぜひご参加ください。',
+    status: 'wanted',
+    url: 'https://www.morikita.co.jp/storage/images/cvr/069071cvr.jpg',
+    span: '週１回',
+    style: 'zoom',
+  },
   {
     name: '電子工作入門ゼミ',
     description:
